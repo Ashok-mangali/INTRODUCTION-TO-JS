78f36c488cd912a99325225db890a1d7a5721e4c
ashok <aashok59238@gmail.com>
2023-12-01 09:51:51 +0530

    Introduction To Java Script

---
+++ INTRODUCTION JS/6.Operator.js
@@ -0,0 +1,9 @@
+// Arithemetic Operators 
+
+let num1 = 2;
+let num2 = 3;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 % num2);
+console.log(num1 / num2);
